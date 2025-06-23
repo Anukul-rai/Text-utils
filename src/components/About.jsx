bc755855769da2356fa73f4c704c626d31e0d658
anukul rai <anukul.rau@gmail.com>
2025-06-23 23:59:08 +0545

    some updates

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,19 +5,30 @@ import { Button } from '../styled/Button'
 function About() {
     
     const [showTopic, setShowTopic] = useState(false)
+    const [myStyle,setMyStyle] = useState({backgroundColor:'#f0f0f0',color:"black"
+    })
+    const [btnText,setBtnText]=useState("Enable Dark mode")
 
-    
+    const toggleDark=()=>{
+        if(myStyle.color =="black"){
+            setMyStyle({backgroundColor:'black',color:"white",border:"1px solid white"})
+            setBtnText("Enable Light mode")
+            }else{  
+            setMyStyle({backgroundColor:'#f0f0f0',color:"black"})
+            setBtnText("Enable Dark mode")
+        }
+    }
   return (
-    <AboutContainer>
+    <AboutContainer style={myStyle}>
       <div className='head'>
         <h1>About us</h1>
-        <div className="drop">
+        <div className="drop" style={myStyle}>
             <h3>Topic 1</h3>
             <Button onClick={() => setShowTopic(!showTopic)}>
             {showTopic ? 'Close' : 'Open'}
             </Button>
             {showTopic && (
-                <p>
+                <p >
                 Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
                 Iste, explicabo quas harum beatae fuga quod quo corrupti 
                 nihil inventore assumenda numquam vero maxime soluta nat, 
@@ -26,7 +37,7 @@ function About() {
             )}
         </div>
         <div className='.btn'>
-        <Button>Enable Dark Mode</Button>
+        <Button onClick={toggleDark}>{btnText}</Button>
         </div>
       </div>
     </AboutContainer>
@@ -49,6 +60,12 @@ const AboutContainer =styled.div`
             display: flex;
             flex-direction: column;
             margin-bottom: 20px;
+            border: 1px dashed black;
+            padding: 10px;
+            border-radius: 10px;
+            h3{
+                margin-bottom: 10px;
+                }
         }
         
     }
